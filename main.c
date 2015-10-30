57e24514586ca34d5b661ba81859cc6768238c68
Aleena Chaudhry <B00074979>
2015-10-30 23:52:01 +0000

    Programming 2 - Lab3 Part1

---
+++ main.c
@@ -0,0 +1,26 @@
+/*LAB 3 Q1 */
+
+/* ------------------------------------------------------------
+
+Programmer: Aleena Chaudhry
+
+ID: B00074979
+
+Date: 3rd October 2014
+
+Purpose: functions
+
+------------------------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   double triangle_area(double base, double height);
+   double product, base, height;
+     {
+       product = base * height;
+       return product / 2;
+    }
+}
+
